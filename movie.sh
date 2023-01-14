0c6c9a4c949d7b5d4b7c03e18e8b9fd16296593b
test-heck <122218742+test-heck@users.noreply.github.com>
2023-01-14 22:04:14 +0545

    Update movie.sh

--- movie.sh
+++ movie.sh
@@ -2,7 +2,7 @@ movie=Dr.Strange
 
 if [[ $(curl https://api.qfxcinemas.com/api/public/banners | jq -r '.data[] | select (.id==7721) | .buyTicket') =~ .*"true".* ]];
 then
-	curl -X POST --data-urlencode "payload={\"channel\": \"#test\", \"username\": \"webhookbot\", \"text\": \"This is posted to #test and $movie ayo\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
+	curl -X POST --data-urlencode "payload={\"channel\": \"#test\", \"username\": \"webhookbot\", \"text\": \"This is posted to #test and $movie ayo\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/0000000000000000/B03DJ365000000000000000000L3A/000
 	echo 'yes'
 else
 	echo 'not available yet'
